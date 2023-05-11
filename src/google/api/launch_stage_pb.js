f5b4dfd73db3962db69a2de3d1e151b3b158cb2b
TCN CI <ci@tcnp3.com>
2023-05-11 06:52:55 +0000

    update tcnapi-grpc-web format

--- src/google/api/launch_stage_pb.js
+++ src/google/api/launch_stage_pb.js
@@ -1,4 +1,4 @@
-// source: permissions/v1/perms.proto
+// source: google/api/launch_stage.proto
 /**
  * @fileoverview
  * @enhanceable
@@ -21,3 +21,19 @@ var global =
     (function () { return this; }).call(null) ||
     Function('return this')();
 
+goog.exportSymbol('proto.google.api.LaunchStage', null, global);
+/**
+ * @enum {number}
+ */
+proto.google.api.LaunchStage = {
+  LAUNCH_STAGE_UNSPECIFIED: 0,
+  UNIMPLEMENTED: 6,
+  PRELAUNCH: 7,
+  EARLY_ACCESS: 1,
+  ALPHA: 2,
+  BETA: 3,
+  GA: 4,
+  DEPRECATED: 5
+};
+
+goog.object.extend(exports, proto.google.api);
