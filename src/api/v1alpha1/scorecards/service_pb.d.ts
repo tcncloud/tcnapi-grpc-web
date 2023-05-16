1226370b5984314515ea9a3d3ba1d8abb6fc9f39
TCN CI <ci@tcnp3.com>
2023-05-16 15:06:36 +0000

    update tcnapi-grpc-web Merge branch '5-add-authz-to-scorecards-protos' into 'master'
    Resolve "Add authz to scorecards protos"
    
    Closes #5
    
    See merge request tcnpublic/api!12

--- src/api/v1alpha1/scorecards/service_pb.d.ts
+++ src/api/v1alpha1/scorecards/service_pb.d.ts
@@ -2,6 +2,7 @@
 // file: api/v1alpha1/scorecards/service.proto
 
 import * as jspb from "google-protobuf";
+import * as annotations_authz_pb from "../../../annotations/authz_pb";
 import * as api_v1alpha1_scorecards_auto_evaluation_pb from "../../../api/v1alpha1/scorecards/auto_evaluation_pb";
 import * as api_v1alpha1_scorecards_auto_question_pb from "../../../api/v1alpha1/scorecards/auto_question_pb";
 import * as api_v1alpha1_scorecards_category_pb from "../../../api/v1alpha1/scorecards/category_pb";
