d42918c93aa69936982b9aae14a6bb6edddc00bf
TCN CI <ci@tcnp3.com>
2023-06-09 16:15:00 +0000

    update tcnapi-grpc-web > Update Learning center permissions

--- src/api/v0alpha/learn_pb.d.ts
+++ src/api/v0alpha/learn_pb.d.ts
@@ -2,6 +2,8 @@
 // file: api/v0alpha/learn.proto
 
 import * as jspb from "google-protobuf";
+import * as annotations_authz_pb from "../../annotations/authz_pb";
+import * as google_api_annotations_pb from "../../google/api/annotations_pb";
 import * as google_protobuf_field_mask_pb from "google-protobuf/google/protobuf/field_mask_pb";
 import * as google_protobuf_timestamp_pb from "google-protobuf/google/protobuf/timestamp_pb";
 
@@ -520,6 +522,48 @@ export namespace LearnStandaloneDetails {
     name: string,
     content: string,
     lastEditedTimestamp?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+  }
+}
+
+export class DeleteStandaloneReq extends jspb.Message {
+  getLocale(): string;
+  setLocale(value: string): void;
+
+  clearArticleNamesList(): void;
+  getArticleNamesList(): Array<string>;
+  setArticleNamesList(value: Array<string>): void;
+  addArticleNames(value: string, index?: number): string;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): DeleteStandaloneReq.AsObject;
+  static toObject(includeInstance: boolean, msg: DeleteStandaloneReq): DeleteStandaloneReq.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: DeleteStandaloneReq, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): DeleteStandaloneReq;
+  static deserializeBinaryFromReader(message: DeleteStandaloneReq, reader: jspb.BinaryReader): DeleteStandaloneReq;
+}
+
+export namespace DeleteStandaloneReq {
+  export type AsObject = {
+    locale: string,
+    articleNamesList: Array<string>,
+  }
+}
+
+export class DeleteStandaloneRes extends jspb.Message {
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): DeleteStandaloneRes.AsObject;
+  static toObject(includeInstance: boolean, msg: DeleteStandaloneRes): DeleteStandaloneRes.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: DeleteStandaloneRes, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): DeleteStandaloneRes;
+  static deserializeBinaryFromReader(message: DeleteStandaloneRes, reader: jspb.BinaryReader): DeleteStandaloneRes;
+}
+
+export namespace DeleteStandaloneRes {
+  export type AsObject = {
   }
 }
 
--- src/api/v0alpha/learn_pb_service.js
+++ src/api/v0alpha/learn_pb_service.js
@@ -28,6 +28,33 @@ Learn.Content = {
   responseType: api_v0alpha_learn_pb.ContentRes
 };
 
+Learn.ExportMany = {
+  methodName: "ExportMany",
+  service: Learn,
+  requestStream: false,
+  responseStream: false,
+  requestType: api_v0alpha_learn_pb.ExportManyReq,
+  responseType: api_v0alpha_learn_pb.ExportRes
+};
+
+Learn.SearchContent = {
+  methodName: "SearchContent",
+  service: Learn,
+  requestStream: false,
+  responseStream: false,
+  requestType: api_v0alpha_learn_pb.SearchContentReq,
+  responseType: api_v0alpha_learn_pb.SearchRes
+};
+
+Learn.Standalone = {
+  methodName: "Standalone",
+  service: Learn,
+  requestStream: false,
+  responseStream: false,
+  requestType: api_v0alpha_learn_pb.StandaloneReq,
+  responseType: api_v0alpha_learn_pb.StandaloneRes
+};
+
 Learn.ContentEditorData = {
   methodName: "ContentEditorData",
   service: Learn,
@@ -46,15 +73,6 @@ Learn.Update = {
   responseType: api_v0alpha_learn_pb.UpdateRes
 };
 
-Learn.ExportMany = {
-  methodName: "ExportMany",
-  service: Learn,
-  requestStream: false,
-  responseStream: false,
-  requestType: api_v0alpha_learn_pb.ExportManyReq,
-  responseType: api_v0alpha_learn_pb.ExportRes
-};
-
 Learn.StoreStaticImage = {
   methodName: "StoreStaticImage",
   service: Learn,
@@ -62,15 +80,6 @@ Learn.StoreStaticImage = {
   responseStream: false,
   requestType: api_v0alpha_learn_pb.StoreStaticImageReq,
   responseType: api_v0alpha_learn_pb.StoreStaticImageRes
-};
-
-Learn.SearchContent = {
-  methodName: "SearchContent",
-  service: Learn,
-  requestStream: false,
-  responseStream: false,
-  requestType: api_v0alpha_learn_pb.SearchContentReq,
-  responseType: api_v0alpha_learn_pb.SearchRes
 };
 
 Learn.UploadDynamicScreenshot = {
@@ -82,13 +91,13 @@ Learn.UploadDynamicScreenshot = {
   responseType: api_v0alpha_learn_pb.UploadDynamicScreenshotRes
 };
 
-Learn.Standalone = {
-  methodName: "Standalone",
+Learn.DeleteStandalone = {
+  methodName: "DeleteStandalone",
   service: Learn,
   requestStream: false,
   responseStream: false,
-  requestType: api_v0alpha_learn_pb.StandaloneReq,
-  responseType: api_v0alpha_learn_pb.StandaloneRes
+  requestType: api_v0alpha_learn_pb.DeleteStandaloneReq,
+  responseType: api_v0alpha_learn_pb.DeleteStandaloneRes
 };
 
 exports.Learn = Learn;
@@ -134,6 +143,99 @@ LearnClient.prototype.content = function content(requestMessage, metadata, callb
     callback = arguments[1];
   }
   var client = grpc.unary(Learn.Content, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+LearnClient.prototype.exportMany = function exportMany(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Learn.ExportMany, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+LearnClient.prototype.searchContent = function searchContent(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Learn.SearchContent, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+LearnClient.prototype.standalone = function standalone(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Learn.Standalone, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
@@ -222,73 +324,11 @@ LearnClient.prototype.update = function update(requestMessage, metadata, callbac
   };
 };
 
-LearnClient.prototype.exportMany = function exportMany(requestMessage, metadata, callback) {
-  if (arguments.length === 2) {
-    callback = arguments[1];
-  }
-  var client = grpc.unary(Learn.ExportMany, {
-    request: requestMessage,
-    host: this.serviceHost,
-    metadata: metadata,
-    transport: this.options.transport,
-    debug: this.options.debug,
-    onEnd: function (response) {
-      if (callback) {
-        if (response.status !== grpc.Code.OK) {
-          var err = new Error(response.statusMessage);
-          err.code = response.status;
-          err.metadata = response.trailers;
-          callback(err, null);
-        } else {
-          callback(null, response.message);
-        }
-      }
-    }
-  });
-  return {
-    cancel: function () {
-      callback = null;
-      client.close();
-    }
-  };
-};
-
 LearnClient.prototype.storeStaticImage = function storeStaticImage(requestMessage, metadata, callback) {
   if (arguments.length === 2) {
     callback = arguments[1];
   }
   var client = grpc.unary(Learn.StoreStaticImage, {
-    request: requestMessage,
-    host: this.serviceHost,
-    metadata: metadata,
-    transport: this.options.transport,
-    debug: this.options.debug,
-    onEnd: function (response) {
-      if (callback) {
-        if (response.status !== grpc.Code.OK) {
-          var err = new Error(response.statusMessage);
-          err.code = response.status;
-          err.metadata = response.trailers;
-          callback(err, null);
-        } else {
-          callback(null, response.message);
-        }
-      }
-    }
-  });
-  return {
-    cancel: function () {
-      callback = null;
-      client.close();
-    }
-  };
-};
-
-LearnClient.prototype.searchContent = function searchContent(requestMessage, metadata, callback) {
-  if (arguments.length === 2) {
-    callback = arguments[1];
-  }
-  var client = grpc.unary(Learn.SearchContent, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
@@ -346,11 +386,11 @@ LearnClient.prototype.uploadDynamicScreenshot = function uploadDynamicScreenshot
   };
 };
 
-LearnClient.prototype.standalone = function standalone(requestMessage, metadata, callback) {
+LearnClient.prototype.deleteStandalone = function deleteStandalone(requestMessage, metadata, callback) {
   if (arguments.length === 2) {
     callback = arguments[1];
   }
-  var client = grpc.unary(Learn.Standalone, {
+  var client = grpc.unary(Learn.DeleteStandalone, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
