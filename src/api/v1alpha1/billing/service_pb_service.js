e8080b5aae5b4d0144b084d49de8f088577f99cc
TCN CI <ci@tcnp3.com>
2023-06-14 14:27:31 +0000

    update tcnapi-grpc-web > Merge branch '46-update-billing-api' into 'master'
    Resolve "Update billing api"
    
    Closes #46
    
    See merge request tcnpublic/api!50

--- src/api/v1alpha1/billing/service_pb_service.js
+++ src/api/v1alpha1/billing/service_pb_service.js
@@ -11,15 +11,6 @@ var Billing = (function () {
   return Billing;
 }());
 
-Billing.CreateBillingPlan = {
-  methodName: "CreateBillingPlan",
-  service: Billing,
-  requestStream: false,
-  responseStream: false,
-  requestType: api_v1alpha1_billing_entities_pb.CreateBillingPlanReq,
-  responseType: api_v1alpha1_billing_entities_pb.CreateBillingPlanRes
-};
-
 Billing.GetBillingPlan = {
   methodName: "GetBillingPlan",
   service: Billing,
@@ -38,15 +29,6 @@ Billing.UpdateBillingPlan = {
   responseType: api_v1alpha1_billing_entities_pb.UpdateBillingPlanRes
 };
 
-Billing.DeleteBillingDetails = {
-  methodName: "DeleteBillingDetails",
-  service: Billing,
-  requestStream: false,
-  responseStream: false,
-  requestType: api_v1alpha1_billing_entities_pb.DeleteBillingDetailsReq,
-  responseType: api_v1alpha1_billing_entities_pb.DeleteBillingDetailsRes
-};
-
 Billing.GetInvoice = {
   methodName: "GetInvoice",
   service: Billing,
@@ -62,37 +44,6 @@ function BillingClient(serviceHost, options) {
   this.serviceHost = serviceHost;
   this.options = options || {};
 }
-
-BillingClient.prototype.createBillingPlan = function createBillingPlan(requestMessage, metadata, callback) {
-  if (arguments.length === 2) {
-    callback = arguments[1];
-  }
-  var client = grpc.unary(Billing.CreateBillingPlan, {
-    request: requestMessage,
-    host: this.serviceHost,
-    metadata: metadata,
-    transport: this.options.transport,
-    debug: this.options.debug,
-    onEnd: function (response) {
-      if (callback) {
-        if (response.status !== grpc.Code.OK) {
-          var err = new Error(response.statusMessage);
-          err.code = response.status;
-          err.metadata = response.trailers;
-          callback(err, null);
-        } else {
-          callback(null, response.message);
-        }
-      }
-    }
-  });
-  return {
-    cancel: function () {
-      callback = null;
-      client.close();
-    }
-  };
-};
 
 BillingClient.prototype.getBillingPlan = function getBillingPlan(requestMessage, metadata, callback) {
   if (arguments.length === 2) {
@@ -130,37 +81,6 @@ BillingClient.prototype.updateBillingPlan = function updateBillingPlan(requestMe
     callback = arguments[1];
   }
   var client = grpc.unary(Billing.UpdateBillingPlan, {
-    request: requestMessage,
-    host: this.serviceHost,
-    metadata: metadata,
-    transport: this.options.transport,
-    debug: this.options.debug,
-    onEnd: function (response) {
-      if (callback) {
-        if (response.status !== grpc.Code.OK) {
-          var err = new Error(response.statusMessage);
-          err.code = response.status;
-          err.metadata = response.trailers;
-          callback(err, null);
-        } else {
-          callback(null, response.message);
-        }
-      }
-    }
-  });
-  return {
-    cancel: function () {
-      callback = null;
-      client.close();
-    }
-  };
-};
-
-BillingClient.prototype.deleteBillingDetails = function deleteBillingDetails(requestMessage, metadata, callback) {
-  if (arguments.length === 2) {
-    callback = arguments[1];
-  }
-  var client = grpc.unary(Billing.DeleteBillingDetails, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
