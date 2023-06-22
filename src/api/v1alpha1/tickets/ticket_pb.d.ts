2d5b9e07d09d53fc3869b5c6b3f560a119b66974
TCN CI <ci@tcnp3.com>
2023-06-22 15:21:14 +0000

    update tcnapi-grpc-web > Merge branch 'master' into 62-cleanup-workflows-protobufs-to-remove-unused-logic-and-update-naming-convention

--- src/api/v1alpha1/tickets/ticket_pb.d.ts
+++ src/api/v1alpha1/tickets/ticket_pb.d.ts
@@ -73,6 +73,9 @@ export class CreateTicketReq extends jspb.Message {
   getAssignSelf(): boolean;
   setAssignSelf(value: boolean): void;
 
+  getAssignOther(): string;
+  setAssignOther(value: string): void;
+
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): CreateTicketReq.AsObject;
   static toObject(includeInstance: boolean, msg: CreateTicketReq): CreateTicketReq.AsObject;
@@ -94,6 +97,7 @@ export namespace CreateTicketReq {
     status: number,
     ticketSlaList: Array<api_commons_tickets_pb.Sla.AsObject>,
     assignSelf: boolean,
+    assignOther: string,
   }
 }
 
@@ -120,8 +124,8 @@ export namespace CreateTicketRes {
 }
 
 export class EditTicketReq extends jspb.Message {
-  getTicketSid(): number;
-  setTicketSid(value: number): void;
+  getTicketSid(): string;
+  setTicketSid(value: string): void;
 
   hasEditValue(): boolean;
   clearEditValue(): void;
@@ -140,7 +144,7 @@ export class EditTicketReq extends jspb.Message {
 
 export namespace EditTicketReq {
   export type AsObject = {
-    ticketSid: number,
+    ticketSid: string,
     editValue?: api_commons_tickets_pb.EditAttribute.AsObject,
   }
 }
