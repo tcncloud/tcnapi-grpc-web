23accf4ee38347ddedd01232e2fb2c2df0741138
TCN CI <ci@tcnp3.com>
2023-06-26 18:06:13 +0000

    update tcnapi-grpc-web > Merge branch '61-authconnection-protos' into 'master'
    Resolve "AuthConnection Protos"
    
    Closes #61
    
    See merge request tcnpublic/api!69

---
+++ src/api/v1alpha1/org/authconnection/service_pb.d.ts
@@ -0,0 +1,8 @@
+// package: api.v1alpha1.org.authconnection
+// file: api/v1alpha1/org/authconnection/service.proto
+
+import * as jspb from "google-protobuf";
+import * as annotations_authz_pb from "../../../../annotations/authz_pb";
+import * as api_v1alpha1_org_authconnection_entities_pb from "../../../../api/v1alpha1/org/authconnection/entities_pb";
+import * as google_api_annotations_pb from "../../../../google/api/annotations_pb";
+
