b3ecf29958159d9ac471ea8823bda25998bb6e16
TCN CI <ci@tcnp3.com>
2023-07-24 16:56:37 +0000

    update tcnapi-grpc-web > Merge branch 'master' of https://git.tcncloud.net/tcnpublic/api into 91-learn-update-protos-to-support-for-category-for-standalone-pages

---
+++ src/api/v1alpha1/agenttraining/service_pb.d.ts
@@ -0,0 +1,8 @@
+// package: api.v1alpha1.agenttraining
+// file: api/v1alpha1/agenttraining/service.proto
+
+import * as jspb from "google-protobuf";
+import * as annotations_authz_pb from "../../../annotations/authz_pb";
+import * as api_v1alpha1_agenttraining_learning_opportunity_pb from "../../../api/v1alpha1/agenttraining/learning_opportunity_pb";
+import * as google_api_annotations_pb from "../../../google/api/annotations_pb";
+
