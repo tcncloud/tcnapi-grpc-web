99086585b77908bf5b787af2910dee530901b5e8
TCN CI <ci@tcnp3.com>
2023-08-30 21:33:37 +0000

    update tcnapi-grpc-web > Merge branch '159-newsroom-add-protos-for-news-articles' into 'master'
    Resolve "Newsroom - Add protos for news articles"
    
    Closes #159
    
    See merge request tcnpublic/api!172

---
+++ src/api/commons/newsroom_pb.d.ts
@@ -0,0 +1,13 @@
+// package: api.commons
+// file: api/commons/newsroom.proto
+
+import * as jspb from "google-protobuf";
+
+export interface ArticleStatusMap {
+  STATUS_DRAFT: 0;
+  STATUS_PUBLISHED: 1;
+  STATUS_ARCHIVED: 2;
+}
+
+export const ArticleStatus: ArticleStatusMap;
+
