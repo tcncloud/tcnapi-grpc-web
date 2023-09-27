aff0d22d37cafdd8b94e6f52b1ea1d78724cc4e3
TCN CI <ci@tcnp3.com>
2023-09-27 15:17:06 +0000

    update tcnapi-grpc-web > Merge branch '141-pm-at-add-new-learning-opportunity-endpoints-2' into 'master'
    Resolve "PM-AT Add new learning opportunity endpoints"
    
    Closes #141
    
    See merge request tcnpublic/api!156

--- src/api/v1alpha1/agenttraining/learning_opportunity_pb.d.ts
+++ src/api/v1alpha1/agenttraining/learning_opportunity_pb.d.ts
@@ -3,6 +3,8 @@
 
 import * as jspb from "google-protobuf";
 import * as api_commons_agent_training_pb from "../../../api/commons/agent_training_pb";
+import * as api_commons_scorecards_pb from "../../../api/commons/scorecards_pb";
+import * as google_protobuf_field_mask_pb from "google-protobuf/google/protobuf/field_mask_pb";
 
 export class CreateLearningOpportunityRequest extends jspb.Message {
   hasLearningOpportunity(): boolean;
@@ -49,6 +51,26 @@ export namespace CreateLearningOpportunityResponse {
 }
 
 export class ListLearningOpportunitiesRequest extends jspb.Message {
+  clearCallIdentifiersList(): void;
+  getCallIdentifiersList(): Array<api_commons_agent_training_pb.CallIdentifier>;
+  setCallIdentifiersList(value: Array<api_commons_agent_training_pb.CallIdentifier>): void;
+  addCallIdentifiers(value?: api_commons_agent_training_pb.CallIdentifier, index?: number): api_commons_agent_training_pb.CallIdentifier;
+
+  clearTranscriptSidsList(): void;
+  getTranscriptSidsList(): Array<number>;
+  setTranscriptSidsList(value: Array<number>): void;
+  addTranscriptSids(value: number, index?: number): number;
+
+  clearAgentUserIdsList(): void;
+  getAgentUserIdsList(): Array<string>;
+  setAgentUserIdsList(value: Array<string>): void;
+  addAgentUserIds(value: string, index?: number): string;
+
+  hasCreatedAt(): boolean;
+  clearCreatedAt(): void;
+  getCreatedAt(): api_commons_scorecards_pb.TimeFilter | undefined;
+  setCreatedAt(value?: api_commons_scorecards_pb.TimeFilter): void;
+
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): ListLearningOpportunitiesRequest.AsObject;
   static toObject(includeInstance: boolean, msg: ListLearningOpportunitiesRequest): ListLearningOpportunitiesRequest.AsObject;
@@ -61,6 +83,10 @@ export class ListLearningOpportunitiesRequest extends jspb.Message {
 
 export namespace ListLearningOpportunitiesRequest {
   export type AsObject = {
+    callIdentifiersList: Array<api_commons_agent_training_pb.CallIdentifier.AsObject>,
+    transcriptSidsList: Array<number>,
+    agentUserIdsList: Array<string>,
+    createdAt?: api_commons_scorecards_pb.TimeFilter.AsObject,
   }
 }
 
@@ -83,6 +109,196 @@ export class ListLearningOpportunitiesResponse extends jspb.Message {
 export namespace ListLearningOpportunitiesResponse {
   export type AsObject = {
     learningOpportunitiesList: Array<api_commons_agent_training_pb.LearningOpportunity.AsObject>,
+  }
+}
+
+export class ListAgentLearningOpportunitiesRequest extends jspb.Message {
+  clearCallIdentifiersList(): void;
+  getCallIdentifiersList(): Array<api_commons_agent_training_pb.CallIdentifier>;
+  setCallIdentifiersList(value: Array<api_commons_agent_training_pb.CallIdentifier>): void;
+  addCallIdentifiers(value?: api_commons_agent_training_pb.CallIdentifier, index?: number): api_commons_agent_training_pb.CallIdentifier;
+
+  clearTranscriptSidsList(): void;
+  getTranscriptSidsList(): Array<number>;
+  setTranscriptSidsList(value: Array<number>): void;
+  addTranscriptSids(value: number, index?: number): number;
+
+  hasCreatedAt(): boolean;
+  clearCreatedAt(): void;
+  getCreatedAt(): api_commons_scorecards_pb.TimeFilter | undefined;
+  setCreatedAt(value?: api_commons_scorecards_pb.TimeFilter): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ListAgentLearningOpportunitiesRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: ListAgentLearningOpportunitiesRequest): ListAgentLearningOpportunitiesRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ListAgentLearningOpportunitiesRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ListAgentLearningOpportunitiesRequest;
+  static deserializeBinaryFromReader(message: ListAgentLearningOpportunitiesRequest, reader: jspb.BinaryReader): ListAgentLearningOpportunitiesRequest;
+}
+
+export namespace ListAgentLearningOpportunitiesRequest {
+  export type AsObject = {
+    callIdentifiersList: Array<api_commons_agent_training_pb.CallIdentifier.AsObject>,
+    transcriptSidsList: Array<number>,
+    createdAt?: api_commons_scorecards_pb.TimeFilter.AsObject,
+  }
+}
+
+export class ListAgentLearningOpportunitiesResponse extends jspb.Message {
+  clearLearningOpportunitiesList(): void;
+  getLearningOpportunitiesList(): Array<api_commons_agent_training_pb.LearningOpportunity>;
+  setLearningOpportunitiesList(value: Array<api_commons_agent_training_pb.LearningOpportunity>): void;
+  addLearningOpportunities(value?: api_commons_agent_training_pb.LearningOpportunity, index?: number): api_commons_agent_training_pb.LearningOpportunity;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ListAgentLearningOpportunitiesResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: ListAgentLearningOpportunitiesResponse): ListAgentLearningOpportunitiesResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ListAgentLearningOpportunitiesResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ListAgentLearningOpportunitiesResponse;
+  static deserializeBinaryFromReader(message: ListAgentLearningOpportunitiesResponse, reader: jspb.BinaryReader): ListAgentLearningOpportunitiesResponse;
+}
+
+export namespace ListAgentLearningOpportunitiesResponse {
+  export type AsObject = {
+    learningOpportunitiesList: Array<api_commons_agent_training_pb.LearningOpportunity.AsObject>,
+  }
+}
+
+export class UpdateLearningOpportunityRequest extends jspb.Message {
+  hasLearningOpportunity(): boolean;
+  clearLearningOpportunity(): void;
+  getLearningOpportunity(): api_commons_agent_training_pb.LearningOpportunity | undefined;
+  setLearningOpportunity(value?: api_commons_agent_training_pb.LearningOpportunity): void;
+
+  hasUpdateMask(): boolean;
+  clearUpdateMask(): void;
+  getUpdateMask(): google_protobuf_field_mask_pb.FieldMask | undefined;
+  setUpdateMask(value?: google_protobuf_field_mask_pb.FieldMask): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): UpdateLearningOpportunityRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: UpdateLearningOpportunityRequest): UpdateLearningOpportunityRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: UpdateLearningOpportunityRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): UpdateLearningOpportunityRequest;
+  static deserializeBinaryFromReader(message: UpdateLearningOpportunityRequest, reader: jspb.BinaryReader): UpdateLearningOpportunityRequest;
+}
+
+export namespace UpdateLearningOpportunityRequest {
+  export type AsObject = {
+    learningOpportunity?: api_commons_agent_training_pb.LearningOpportunity.AsObject,
+    updateMask?: google_protobuf_field_mask_pb.FieldMask.AsObject,
+  }
+}
+
+export class UpdateLearningOpportunityResponse extends jspb.Message {
+  hasLearningOpportunity(): boolean;
+  clearLearningOpportunity(): void;
+  getLearningOpportunity(): api_commons_agent_training_pb.LearningOpportunity | undefined;
+  setLearningOpportunity(value?: api_commons_agent_training_pb.LearningOpportunity): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): UpdateLearningOpportunityResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: UpdateLearningOpportunityResponse): UpdateLearningOpportunityResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: UpdateLearningOpportunityResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): UpdateLearningOpportunityResponse;
+  static deserializeBinaryFromReader(message: UpdateLearningOpportunityResponse, reader: jspb.BinaryReader): UpdateLearningOpportunityResponse;
+}
+
+export namespace UpdateLearningOpportunityResponse {
+  export type AsObject = {
+    learningOpportunity?: api_commons_agent_training_pb.LearningOpportunity.AsObject,
+  }
+}
+
+export class DeleteLearningOpportunityRequest extends jspb.Message {
+  getLearningOpportunityId(): number;
+  setLearningOpportunityId(value: number): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): DeleteLearningOpportunityRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: DeleteLearningOpportunityRequest): DeleteLearningOpportunityRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: DeleteLearningOpportunityRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): DeleteLearningOpportunityRequest;
+  static deserializeBinaryFromReader(message: DeleteLearningOpportunityRequest, reader: jspb.BinaryReader): DeleteLearningOpportunityRequest;
+}
+
+export namespace DeleteLearningOpportunityRequest {
+  export type AsObject = {
+    learningOpportunityId: number,
+  }
+}
+
+export class DeleteLearningOpportunityResponse extends jspb.Message {
+  hasLearningOpportunity(): boolean;
+  clearLearningOpportunity(): void;
+  getLearningOpportunity(): api_commons_agent_training_pb.LearningOpportunity | undefined;
+  setLearningOpportunity(value?: api_commons_agent_training_pb.LearningOpportunity): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): DeleteLearningOpportunityResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: DeleteLearningOpportunityResponse): DeleteLearningOpportunityResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: DeleteLearningOpportunityResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): DeleteLearningOpportunityResponse;
+  static deserializeBinaryFromReader(message: DeleteLearningOpportunityResponse, reader: jspb.BinaryReader): DeleteLearningOpportunityResponse;
+}
+
+export namespace DeleteLearningOpportunityResponse {
+  export type AsObject = {
+    learningOpportunity?: api_commons_agent_training_pb.LearningOpportunity.AsObject,
+  }
+}
+
+export class GetLearningOpportunityRequest extends jspb.Message {
+  getLearningOpportunity(): number;
+  setLearningOpportunity(value: number): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetLearningOpportunityRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: GetLearningOpportunityRequest): GetLearningOpportunityRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: GetLearningOpportunityRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetLearningOpportunityRequest;
+  static deserializeBinaryFromReader(message: GetLearningOpportunityRequest, reader: jspb.BinaryReader): GetLearningOpportunityRequest;
+}
+
+export namespace GetLearningOpportunityRequest {
+  export type AsObject = {
+    learningOpportunity: number,
+  }
+}
+
+export class GetLearningOpportunityResponse extends jspb.Message {
+  hasLearningOpportunity(): boolean;
+  clearLearningOpportunity(): void;
+  getLearningOpportunity(): api_commons_agent_training_pb.LearningOpportunity | undefined;
+  setLearningOpportunity(value?: api_commons_agent_training_pb.LearningOpportunity): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetLearningOpportunityResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: GetLearningOpportunityResponse): GetLearningOpportunityResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: GetLearningOpportunityResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetLearningOpportunityResponse;
+  static deserializeBinaryFromReader(message: GetLearningOpportunityResponse, reader: jspb.BinaryReader): GetLearningOpportunityResponse;
+}
+
+export namespace GetLearningOpportunityResponse {
+  export type AsObject = {
+    learningOpportunity?: api_commons_agent_training_pb.LearningOpportunity.AsObject,
   }
 }
 
--- src/api/v1alpha1/agenttraining/service_pb_service.js
+++ src/api/v1alpha1/agenttraining/service_pb_service.js
@@ -29,6 +29,42 @@ AgentTrainingService.ListLearningOpportunities = {
   responseType: api_v1alpha1_agenttraining_learning_opportunity_pb.ListLearningOpportunitiesResponse
 };
 
+AgentTrainingService.ListAgentLearningOpportunities = {
+  methodName: "ListAgentLearningOpportunities",
+  service: AgentTrainingService,
+  requestStream: false,
+  responseStream: false,
+  requestType: api_v1alpha1_agenttraining_learning_opportunity_pb.ListAgentLearningOpportunitiesRequest,
+  responseType: api_v1alpha1_agenttraining_learning_opportunity_pb.ListAgentLearningOpportunitiesResponse
+};
+
+AgentTrainingService.UpdateLearningOpportunity = {
+  methodName: "UpdateLearningOpportunity",
+  service: AgentTrainingService,
+  requestStream: false,
+  responseStream: false,
+  requestType: api_v1alpha1_agenttraining_learning_opportunity_pb.UpdateLearningOpportunityRequest,
+  responseType: api_v1alpha1_agenttraining_learning_opportunity_pb.UpdateLearningOpportunityResponse
+};
+
+AgentTrainingService.DeleteLearningOpportunity = {
+  methodName: "DeleteLearningOpportunity",
+  service: AgentTrainingService,
+  requestStream: false,
+  responseStream: false,
+  requestType: api_v1alpha1_agenttraining_learning_opportunity_pb.DeleteLearningOpportunityRequest,
+  responseType: api_v1alpha1_agenttraining_learning_opportunity_pb.DeleteLearningOpportunityResponse
+};
+
+AgentTrainingService.GetLearningOpportunity = {
+  methodName: "GetLearningOpportunity",
+  service: AgentTrainingService,
+  requestStream: false,
+  responseStream: false,
+  requestType: api_v1alpha1_agenttraining_learning_opportunity_pb.GetLearningOpportunityRequest,
+  responseType: api_v1alpha1_agenttraining_learning_opportunity_pb.GetLearningOpportunityResponse
+};
+
 exports.AgentTrainingService = AgentTrainingService;
 
 function AgentTrainingServiceClient(serviceHost, options) {
@@ -72,6 +108,130 @@ AgentTrainingServiceClient.prototype.listLearningOpportunities = function listLe
     callback = arguments[1];
   }
   var client = grpc.unary(AgentTrainingService.ListLearningOpportunities, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+AgentTrainingServiceClient.prototype.listAgentLearningOpportunities = function listAgentLearningOpportunities(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(AgentTrainingService.ListAgentLearningOpportunities, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+AgentTrainingServiceClient.prototype.updateLearningOpportunity = function updateLearningOpportunity(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(AgentTrainingService.UpdateLearningOpportunity, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+AgentTrainingServiceClient.prototype.deleteLearningOpportunity = function deleteLearningOpportunity(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(AgentTrainingService.DeleteLearningOpportunity, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+AgentTrainingServiceClient.prototype.getLearningOpportunity = function getLearningOpportunity(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(AgentTrainingService.GetLearningOpportunity, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
