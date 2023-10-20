b1817075894e24367a047e2824cc40ce38e31df6
TCN CI <ci@tcnp3.com>
2023-10-20 20:06:11 +0000

    update tcnapi-grpc-web > Merge branch '235-add-search-past-transactions-rpc' into 'master'
    Resolve "add search past transactions rpc"
    
    Closes #235
    
    See merge request tcnpublic/api!259

--- src/api/v1alpha1/integrations/service_pb_service.js
+++ src/api/v1alpha1/integrations/service_pb_service.js
@@ -46,6 +46,15 @@ Integrations.GetIntegrationTransactionReportData = {
   responseType: api_v1alpha1_integrations_service_pb.GetIntegrationTransactionReportDataRes
 };
 
+Integrations.SearchPastTransactions = {
+  methodName: "SearchPastTransactions",
+  service: Integrations,
+  requestStream: false,
+  responseStream: false,
+  requestType: api_v1alpha1_integrations_service_pb.SearchPastTransactionsRequest,
+  responseType: api_v1alpha1_integrations_service_pb.SearchPastTransactionsResponse
+};
+
 Integrations.GetAggregatedMetadata = {
   methodName: "GetAggregatedMetadata",
   service: Integrations,
@@ -340,6 +349,37 @@ IntegrationsClient.prototype.getIntegrationTransactionReportData = function getI
     callback = arguments[1];
   }
   var client = grpc.unary(Integrations.GetIntegrationTransactionReportData, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+IntegrationsClient.prototype.searchPastTransactions = function searchPastTransactions(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Integrations.SearchPastTransactions, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
