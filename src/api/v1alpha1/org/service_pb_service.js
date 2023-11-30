c8fb32b22d86bc52c061e4f67f1ca229d7264e2d
TCN CI <ci@tcnp3.com>
2023-11-30 21:35:18 +0000

    update tcnapi-grpc-web > Merge branch '280-create-authtoken-endpoints' into 'master'
    Resolve "Create AuthToken endpoints"
    
    Closes #280
    
    See merge request tcnpublic/api!303

--- src/api/v1alpha1/org/service_pb_service.js
+++ src/api/v1alpha1/org/service_pb_service.js
@@ -354,15 +354,6 @@ Org.UpdateBusinessPreferences = {
   responseType: api_v1alpha1_org_preferences_pb.UpdateBusinessPreferencesResponse
 };
 
-Org.UpdateAdminBusinessPreferences = {
-  methodName: "UpdateAdminBusinessPreferences",
-  service: Org,
-  requestStream: false,
-  responseStream: false,
-  requestType: api_v1alpha1_org_preferences_pb.UpdateAdminBusinessPreferencesRequest,
-  responseType: api_v1alpha1_org_preferences_pb.UpdateAdminBusinessPreferencesResponse
-};
-
 Org.GetScorecardsPreferences = {
   methodName: "GetScorecardsPreferences",
   service: Org,
@@ -2639,37 +2630,6 @@ OrgClient.prototype.updateBusinessPreferences = function updateBusinessPreferenc
     callback = arguments[1];
   }
   var client = grpc.unary(Org.UpdateBusinessPreferences, {
-    request: requestMessage,
-    host: this.serviceHost,
-    metadata: metadata,
-    transport: this.options.transport,
-    debug: this.options.debug,
-    onEnd: function (response) {
-      if (callback) {
-        if (response.status !== grpc.Code.OK) {
-          var err = new Error(response.statusMessage);
-          err.code = response.status;
-          err.metadata = response.trailers;
-          callback(err, null);
-        } else {
-          callback(null, response.message);
-        }
-      }
-    }
-  });
-  return {
-    cancel: function () {
-      callback = null;
-      client.close();
-    }
-  };
-};
-
-OrgClient.prototype.updateAdminBusinessPreferences = function updateAdminBusinessPreferences(requestMessage, metadata, callback) {
-  if (arguments.length === 2) {
-    callback = arguments[1];
-  }
-  var client = grpc.unary(Org.UpdateAdminBusinessPreferences, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
