5862cec8b18bb937abc28a1b2a154d08591f9a9e
TCN CI <ci@tcnp3.com>
2024-01-23 18:27:40 +0000

    update tcnapi-grpc-web > Merge branch '344-update-billing-protos' into 'master'
    Resolve "Update billing protos"
    
    Closes #344
    
    See merge request tcnpublic/api!368

--- src/services/billing/entities/v1alpha1/matching_pb.d.ts
+++ src/services/billing/entities/v1alpha1/matching_pb.d.ts
@@ -4,10 +4,18 @@
 import * as jspb from "google-protobuf";
 
 export class MatchingConfig extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
   hasAreaCode(): boolean;
   clearAreaCode(): void;
   getAreaCode(): MatchingConfigAreaCode | undefined;
   setAreaCode(value?: MatchingConfigAreaCode): void;
+
+  hasCountryPrefix(): boolean;
+  clearCountryPrefix(): void;
+  getCountryPrefix(): MatchingConfigCountryPrefix | undefined;
+  setCountryPrefix(value?: MatchingConfigCountryPrefix): void;
 
   getConfigCase(): MatchingConfig.ConfigCase;
   serializeBinary(): Uint8Array;
@@ -22,12 +30,15 @@ export class MatchingConfig extends jspb.Message {
 
 export namespace MatchingConfig {
   export type AsObject = {
+    name: string,
     areaCode?: MatchingConfigAreaCode.AsObject,
+    countryPrefix?: MatchingConfigCountryPrefix.AsObject,
   }
 
   export enum ConfigCase {
     CONFIG_NOT_SET = 0,
     AREA_CODE = 1,
+    COUNTRY_PREFIX = 100,
   }
 }
 
@@ -57,9 +68,36 @@ export namespace MatchingConfigAreaCode {
   }
 }
 
+export class MatchingConfigCountryPrefix extends jspb.Message {
+  getCountryCode(): number;
+  setCountryCode(value: number): void;
+
+  clearPrefixesList(): void;
+  getPrefixesList(): Array<string>;
+  setPrefixesList(value: Array<string>): void;
+  addPrefixes(value: string, index?: number): string;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): MatchingConfigCountryPrefix.AsObject;
+  static toObject(includeInstance: boolean, msg: MatchingConfigCountryPrefix): MatchingConfigCountryPrefix.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: MatchingConfigCountryPrefix, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): MatchingConfigCountryPrefix;
+  static deserializeBinaryFromReader(message: MatchingConfigCountryPrefix, reader: jspb.BinaryReader): MatchingConfigCountryPrefix;
+}
+
+export namespace MatchingConfigCountryPrefix {
+  export type AsObject = {
+    countryCode: number,
+    prefixesList: Array<string>,
+  }
+}
+
 export interface MatchingRuleMap {
   MATCHING_RULE_UNSPECIFIED: 0;
   MATCHING_RULE_AREA_CODE: 1;
+  MATCHING_RULE_COUNTRY_PREFIX: 2;
 }
 
 export const MatchingRule: MatchingRuleMap;
