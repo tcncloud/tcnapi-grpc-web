1e332604c322e98693dd4bddd7b183c4a1fb0631
TCN CI <ci@tcnp3.com>
2024-03-05 15:49:50 +0000

    update tcnapi-grpc-web > Review comment changes

--- src/api/v1alpha1/contactmanager/contactmanager_pb.d.ts
+++ src/api/v1alpha1/contactmanager/contactmanager_pb.d.ts
@@ -218,12 +218,12 @@ export namespace GetKYCKeysRequest {
 
 export class GetKYCKeysResponse extends jspb.Message {
   clearEntryValList(): void;
-  getEntryValList(): Array<api_commons_contactmanager_pb.ContactManagerEntryVal>;
-  setEntryValList(value: Array<api_commons_contactmanager_pb.ContactManagerEntryVal>): void;
-  addEntryVal(value?: api_commons_contactmanager_pb.ContactManagerEntryVal, index?: number): api_commons_contactmanager_pb.ContactManagerEntryVal;
+  getEntryValList(): Array<string>;
+  setEntryValList(value: Array<string>): void;
+  addEntryVal(value: string, index?: number): string;
 
-  getMinKycLimit(): number;
-  setMinKycLimit(value: number): void;
+  getKycLimit(): number;
+  setKycLimit(value: number): void;
 
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): GetKYCKeysResponse.AsObject;
@@ -237,8 +237,126 @@ export class GetKYCKeysResponse extends jspb.Message {
 
 export namespace GetKYCKeysResponse {
   export type AsObject = {
-    entryValList: Array<api_commons_contactmanager_pb.ContactManagerEntryVal.AsObject>,
-    minKycLimit: number,
+    entryValList: Array<string>,
+    kycLimit: number,
+  }
+}
+
+export class ContactManagerEntry extends jspb.Message {
+  getContactManagerEntryId(): string;
+  setContactManagerEntryId(value: string): void;
+
+  getContactManagerEntryListId(): string;
+  setContactManagerEntryListId(value: string): void;
+
+  getKey(): string;
+  setKey(value: string): void;
+
+  getValue(): string;
+  setValue(value: string): void;
+
+  getType(): string;
+  setType(value: string): void;
+
+  hasDateCreated(): boolean;
+  clearDateCreated(): void;
+  getDateCreated(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setDateCreated(value?: google_protobuf_timestamp_pb.Timestamp): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ContactManagerEntry.AsObject;
+  static toObject(includeInstance: boolean, msg: ContactManagerEntry): ContactManagerEntry.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ContactManagerEntry, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ContactManagerEntry;
+  static deserializeBinaryFromReader(message: ContactManagerEntry, reader: jspb.BinaryReader): ContactManagerEntry;
+}
+
+export namespace ContactManagerEntry {
+  export type AsObject = {
+    contactManagerEntryId: string,
+    contactManagerEntryListId: string,
+    key: string,
+    value: string,
+    type: string,
+    dateCreated?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+  }
+}
+
+export class ContactManagerList extends jspb.Message {
+  getContactManagerListId(): string;
+  setContactManagerListId(value: string): void;
+
+  getOrgId(): string;
+  setOrgId(value: string): void;
+
+  getProjectId(): string;
+  setProjectId(value: string): void;
+
+  getFileName(): string;
+  setFileName(value: string): void;
+
+  getDescription(): string;
+  setDescription(value: string): void;
+
+  clearListDetailsList(): void;
+  getListDetailsList(): Array<string>;
+  setListDetailsList(value: Array<string>): void;
+  addListDetails(value: string, index?: number): string;
+
+  getTtl(): string;
+  setTtl(value: string): void;
+
+  hasDateCreated(): boolean;
+  clearDateCreated(): void;
+  getDateCreated(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setDateCreated(value?: google_protobuf_timestamp_pb.Timestamp): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ContactManagerList.AsObject;
+  static toObject(includeInstance: boolean, msg: ContactManagerList): ContactManagerList.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ContactManagerList, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ContactManagerList;
+  static deserializeBinaryFromReader(message: ContactManagerList, reader: jspb.BinaryReader): ContactManagerList;
+}
+
+export namespace ContactManagerList {
+  export type AsObject = {
+    contactManagerListId: string,
+    orgId: string,
+    projectId: string,
+    fileName: string,
+    description: string,
+    listDetailsList: Array<string>,
+    ttl: string,
+    dateCreated?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+  }
+}
+
+export class ContactManagerEntryVal extends jspb.Message {
+  getType(): string;
+  setType(value: string): void;
+
+  getValue(): string;
+  setValue(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ContactManagerEntryVal.AsObject;
+  static toObject(includeInstance: boolean, msg: ContactManagerEntryVal): ContactManagerEntryVal.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ContactManagerEntryVal, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ContactManagerEntryVal;
+  static deserializeBinaryFromReader(message: ContactManagerEntryVal, reader: jspb.BinaryReader): ContactManagerEntryVal;
+}
+
+export namespace ContactManagerEntryVal {
+  export type AsObject = {
+    type: string,
+    value: string,
   }
 }
 
