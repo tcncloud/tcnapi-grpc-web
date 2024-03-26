9f0959da8eaf38936990722f90d9a943e620d16a
TCN CI <ci@tcnp3.com>
2024-03-26 20:13:21 +0000

    update tcnapi-grpc-web > Merge branch '456-implement-organizations-businesshours-rpcs-and-entites' into 'master'
    Resolve "Implement Organizations BusinessHours rpcs and entities"
    
    Closes #456
    
    See merge request tcnpublic/api!481

---
+++ src/api/v1alpha1/org/businesshours/service_pb.d.ts
@@ -0,0 +1,8 @@
+// package: api.v1alpha1.org.businesshours
+// file: api/v1alpha1/org/businesshours/service.proto
+
+import * as jspb from "google-protobuf";
+import * as annotations_authz_pb from "../../../../annotations/authz_pb";
+import * as api_v1alpha1_org_businesshours_entities_pb from "../../../../api/v1alpha1/org/businesshours/entities_pb";
+import * as google_api_annotations_pb from "../../../../google/api/annotations_pb";
+
