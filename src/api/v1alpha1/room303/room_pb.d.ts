7dd87e6351aec2cfc08bd2bd3184b5fa0f6ef6d7
TCN CI <ci@tcnp3.com>
2024-03-28 15:28:12 +0000

    update tcnapi-grpc-web > updateroom endpoint

--- src/api/v1alpha1/room303/room_pb.d.ts
+++ src/api/v1alpha1/room303/room_pb.d.ts
@@ -365,3 +365,31 @@ export namespace GetGlobalConfigResponse {
   }
 }
 
+export class UpdateRoomRequest extends jspb.Message {
+  hasRoom(): boolean;
+  clearRoom(): void;
+  getRoom(): api_commons_room303_pb.Room | undefined;
+  setRoom(value?: api_commons_room303_pb.Room): void;
+
+  hasFieldMask(): boolean;
+  clearFieldMask(): void;
+  getFieldMask(): google_protobuf_field_mask_pb.FieldMask | undefined;
+  setFieldMask(value?: google_protobuf_field_mask_pb.FieldMask): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): UpdateRoomRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: UpdateRoomRequest): UpdateRoomRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: UpdateRoomRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): UpdateRoomRequest;
+  static deserializeBinaryFromReader(message: UpdateRoomRequest, reader: jspb.BinaryReader): UpdateRoomRequest;
+}
+
+export namespace UpdateRoomRequest {
+  export type AsObject = {
+    room?: api_commons_room303_pb.Room.AsObject,
+    fieldMask?: google_protobuf_field_mask_pb.FieldMask.AsObject,
+  }
+}
+
