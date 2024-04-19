e95269a4b77cbaa035f32a29380576877c687c11
TCN CI <ci@tcnp3.com>
2024-04-19 18:00:49 +0000

    update tcnapi-grpc-web > fix scorecards protos

--- src/api/v1alpha1/scorecards/support_service_pb.d.ts
+++ src/api/v1alpha1/scorecards/support_service_pb.d.ts
@@ -4,6 +4,7 @@
 import * as jspb from "google-protobuf";
 import * as annotations_authz_pb from "../../../annotations/authz_pb";
 import * as api_v1alpha1_scorecards_auto_evaluation_pb from "../../../api/v1alpha1/scorecards/auto_evaluation_pb";
+import * as api_v1alpha1_scorecards_category_pb from "../../../api/v1alpha1/scorecards/category_pb";
 import * as api_v1alpha1_scorecards_evaluation_pb from "../../../api/v1alpha1/scorecards/evaluation_pb";
 import * as api_v1alpha1_scorecards_scorecard_pb from "../../../api/v1alpha1/scorecards/scorecard_pb";
 import * as google_api_annotations_pb from "../../../google/api/annotations_pb";
