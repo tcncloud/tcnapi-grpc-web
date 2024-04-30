dc0020886946a173bb07e3cfe9ddd81e843ed02d
TCN CI <ci@tcnp3.com>
2024-04-30 20:20:53 +0000

    update tcnapi-grpc-web > Merge branch '510-update-billing-rate-config' into 'master'
    Resolve "Update billing rate config"
    
    Closes #510
    
    See merge request tcnpublic/api!536

--- src/wfo/vanalytics/v2/service_pb_service.d.ts
+++ src/wfo/vanalytics/v2/service_pb_service.d.ts
@@ -17,6 +17,15 @@ type VanalyticsSearchTranscripts = {
   readonly responseType: typeof wfo_vanalytics_v2_transcript_pb.SearchTranscriptsResponse;
 };
 
+type VanalyticsBulkDeleteTranscripts = {
+  readonly methodName: string;
+  readonly service: typeof Vanalytics;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof wfo_vanalytics_v2_transcript_pb.BulkDeleteTranscriptsRequest;
+  readonly responseType: typeof wfo_vanalytics_v2_transcript_pb.BulkDeleteTranscriptsResponse;
+};
+
 type VanalyticsCreateFilter = {
   readonly methodName: string;
   readonly service: typeof Vanalytics;
@@ -83,6 +92,7 @@ type VanalyticsListFlagFilters = {
 export class Vanalytics {
   static readonly serviceName: string;
   static readonly SearchTranscripts: VanalyticsSearchTranscripts;
+  static readonly BulkDeleteTranscripts: VanalyticsBulkDeleteTranscripts;
   static readonly CreateFilter: VanalyticsCreateFilter;
   static readonly ListFilters: VanalyticsListFilters;
   static readonly UpdateFilter: VanalyticsUpdateFilter;
@@ -132,6 +142,15 @@ export class VanalyticsClient {
   searchTranscripts(
     requestMessage: wfo_vanalytics_v2_transcript_pb.SearchTranscriptsRequest,
     callback: (error: ServiceError|null, responseMessage: wfo_vanalytics_v2_transcript_pb.SearchTranscriptsResponse|null) => void
+  ): UnaryResponse;
+  bulkDeleteTranscripts(
+    requestMessage: wfo_vanalytics_v2_transcript_pb.BulkDeleteTranscriptsRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: wfo_vanalytics_v2_transcript_pb.BulkDeleteTranscriptsResponse|null) => void
+  ): UnaryResponse;
+  bulkDeleteTranscripts(
+    requestMessage: wfo_vanalytics_v2_transcript_pb.BulkDeleteTranscriptsRequest,
+    callback: (error: ServiceError|null, responseMessage: wfo_vanalytics_v2_transcript_pb.BulkDeleteTranscriptsResponse|null) => void
   ): UnaryResponse;
   createFilter(
     requestMessage: wfo_vanalytics_v2_filter_pb.CreateFilterRequest,
