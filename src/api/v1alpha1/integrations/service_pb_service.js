0bd0973bac863fcd49c1b850e7286de2a46fa6d7
TCN CI <ci@tcnp3.com>
2024-05-14 21:48:27 +0000

    update tcnapi-grpc-web > using correct import

--- src/api/v1alpha1/integrations/service_pb_service.js
+++ src/api/v1alpha1/integrations/service_pb_service.js
@@ -271,6 +271,15 @@ Integrations.GenerateEpicKeyPairs = {
   responseType: api_v1alpha1_integrations_service_pb.GenerateEpicKeyPairRes
 };
 
+Integrations.PopulateIntegrationLink = {
+  methodName: "PopulateIntegrationLink",
+  service: Integrations,
+  requestStream: false,
+  responseStream: false,
+  requestType: api_v1alpha1_integrations_service_pb.PopulateIntegrationLinkReq,
+  responseType: api_v1alpha1_integrations_service_pb.PopulateIntegrationLinkRes
+};
+
 exports.Integrations = Integrations;
 
 function IntegrationsClient(serviceHost, options) {
@@ -1151,6 +1160,37 @@ IntegrationsClient.prototype.generateEpicKeyPairs = function generateEpicKeyPair
     callback = arguments[1];
   }
   var client = grpc.unary(Integrations.GenerateEpicKeyPairs, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+IntegrationsClient.prototype.populateIntegrationLink = function populateIntegrationLink(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Integrations.PopulateIntegrationLink, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
