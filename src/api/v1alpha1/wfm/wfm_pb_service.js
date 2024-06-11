f38785cbdd1b6d888f4ad112782b663edb23534c
TCN CI <ci@tcnp3.com>
2024-06-11 20:28:00 +0000

    update tcnapi-grpc-web > Merge remote-tracking branch 'refs/remotes/origin/master'

--- src/api/v1alpha1/wfm/wfm_pb_service.js
+++ src/api/v1alpha1/wfm/wfm_pb_service.js
@@ -1540,6 +1540,15 @@ WFM.ListAgentLeavePetitions = {
   responseType: api_v1alpha1_wfm_wfm_pb.ListAgentLeavePetitionsResponse
 };
 
+WFM.ArchiveAgentLeavePetition = {
+  methodName: "ArchiveAgentLeavePetition",
+  service: WFM,
+  requestStream: false,
+  responseStream: false,
+  requestType: api_v1alpha1_wfm_wfm_pb.ArchiveAgentLeavePetitionRequest,
+  responseType: api_v1alpha1_wfm_wfm_pb.ArchiveAgentLeavePetitionResponse
+};
+
 WFM.HelloWorldWFMAdherence = {
   methodName: "HelloWorldWFMAdherence",
   service: WFM,
@@ -6875,6 +6884,37 @@ WFMClient.prototype.listAgentLeavePetitions = function listAgentLeavePetitions(r
     callback = arguments[1];
   }
   var client = grpc.unary(WFM.ListAgentLeavePetitions, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+WFMClient.prototype.archiveAgentLeavePetition = function archiveAgentLeavePetition(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(WFM.ArchiveAgentLeavePetition, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
