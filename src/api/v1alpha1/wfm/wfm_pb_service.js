c02b28d3c2bdae9edddb998097f43e7572109de0
TCN CI <ci@tcnp3.com>
2024-06-25 20:36:09 +0000

    update tcnapi-grpc-web > Merge branch '494-wfo-ca-add-sentiment-to-transcript-messages' into 'master'
    Resolve "WFO-CA Add sentiment to transcript messages"
    
    Closes #494
    
    See merge request tcnpublic/api!646

--- src/api/v1alpha1/wfm/wfm_pb_service.js
+++ src/api/v1alpha1/wfm/wfm_pb_service.js
@@ -577,15 +577,6 @@ WFM.ListCandidateSchedulingActivities = {
   responseType: api_v1alpha1_wfm_wfm_pb.ListCandidateSchedulingActivitiesRes
 };
 
-WFM.GetOnCallSchedulingActivity = {
-  methodName: "GetOnCallSchedulingActivity",
-  service: WFM,
-  requestStream: false,
-  responseStream: false,
-  requestType: api_v1alpha1_wfm_wfm_pb.GetOnCallSchedulingActivityReq,
-  responseType: api_v1alpha1_wfm_wfm_pb.GetOnCallSchedulingActivityRes
-};
-
 WFM.CreateAgentGroup = {
   methodName: "CreateAgentGroup",
   service: WFM,
@@ -3576,37 +3567,6 @@ WFMClient.prototype.listCandidateSchedulingActivities = function listCandidateSc
     callback = arguments[1];
   }
   var client = grpc.unary(WFM.ListCandidateSchedulingActivities, {
-    request: requestMessage,
-    host: this.serviceHost,
-    metadata: metadata,
-    transport: this.options.transport,
-    debug: this.options.debug,
-    onEnd: function (response) {
-      if (callback) {
-        if (response.status !== grpc.Code.OK) {
-          var err = new Error(response.statusMessage);
-          err.code = response.status;
-          err.metadata = response.trailers;
-          callback(err, null);
-        } else {
-          callback(null, response.message);
-        }
-      }
-    }
-  });
-  return {
-    cancel: function () {
-      callback = null;
-      client.close();
-    }
-  };
-};
-
-WFMClient.prototype.getOnCallSchedulingActivity = function getOnCallSchedulingActivity(requestMessage, metadata, callback) {
-  if (arguments.length === 2) {
-    callback = arguments[1];
-  }
-  var client = grpc.unary(WFM.GetOnCallSchedulingActivity, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
