e9e7c6f3189e0b857bcdea381f1d90e194335406
TCN CI <ci@tcnp3.com>
2024-07-02 18:23:31 +0000

    update tcnapi-grpc-web > Merge branch '599-contact-manager-update-proto-to-support-number-of-successful-and-failed-records-for-cm-audit' into 'master'
    Resolve "Contact Manager - Update Proto to support number of successful and failed records for CM Audit Logs"
    
    Closes #599
    
    See merge request tcnpublic/api!655

--- src/api/v1alpha1/wfm/wfm_pb_service.js
+++ src/api/v1alpha1/wfm/wfm_pb_service.js
@@ -1558,24 +1558,6 @@ WFM.ArchiveAgentLeavePetition = {
   responseType: api_v1alpha1_wfm_wfm_pb.ArchiveAgentLeavePetitionResponse
 };
 
-WFM.ResolveAgentLeavePetition = {
-  methodName: "ResolveAgentLeavePetition",
-  service: WFM,
-  requestStream: false,
-  responseStream: false,
-  requestType: api_v1alpha1_wfm_wfm_pb.ResolveAgentLeavePetitionRequest,
-  responseType: api_v1alpha1_wfm_wfm_pb.ResolveAgentLeavePetitionResponse
-};
-
-WFM.CancelAgentLeavePetition = {
-  methodName: "CancelAgentLeavePetition",
-  service: WFM,
-  requestStream: false,
-  responseStream: false,
-  requestType: api_v1alpha1_wfm_wfm_pb.CancelAgentLeavePetitionRequest,
-  responseType: api_v1alpha1_wfm_wfm_pb.CancelAgentLeavePetitionResponse
-};
-
 WFM.HelloWorldWFMAdherence = {
   methodName: "HelloWorldWFMAdherence",
   service: WFM,
@@ -6973,68 +6955,6 @@ WFMClient.prototype.archiveAgentLeavePetition = function archiveAgentLeavePetiti
     callback = arguments[1];
   }
   var client = grpc.unary(WFM.ArchiveAgentLeavePetition, {
-    request: requestMessage,
-    host: this.serviceHost,
-    metadata: metadata,
-    transport: this.options.transport,
-    debug: this.options.debug,
-    onEnd: function (response) {
-      if (callback) {
-        if (response.status !== grpc.Code.OK) {
-          var err = new Error(response.statusMessage);
-          err.code = response.status;
-          err.metadata = response.trailers;
-          callback(err, null);
-        } else {
-          callback(null, response.message);
-        }
-      }
-    }
-  });
-  return {
-    cancel: function () {
-      callback = null;
-      client.close();
-    }
-  };
-};
-
-WFMClient.prototype.resolveAgentLeavePetition = function resolveAgentLeavePetition(requestMessage, metadata, callback) {
-  if (arguments.length === 2) {
-    callback = arguments[1];
-  }
-  var client = grpc.unary(WFM.ResolveAgentLeavePetition, {
-    request: requestMessage,
-    host: this.serviceHost,
-    metadata: metadata,
-    transport: this.options.transport,
-    debug: this.options.debug,
-    onEnd: function (response) {
-      if (callback) {
-        if (response.status !== grpc.Code.OK) {
-          var err = new Error(response.statusMessage);
-          err.code = response.status;
-          err.metadata = response.trailers;
-          callback(err, null);
-        } else {
-          callback(null, response.message);
-        }
-      }
-    }
-  });
-  return {
-    cancel: function () {
-      callback = null;
-      client.close();
-    }
-  };
-};
-
-WFMClient.prototype.cancelAgentLeavePetition = function cancelAgentLeavePetition(requestMessage, metadata, callback) {
-  if (arguments.length === 2) {
-    callback = arguments[1];
-  }
-  var client = grpc.unary(WFM.CancelAgentLeavePetition, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
