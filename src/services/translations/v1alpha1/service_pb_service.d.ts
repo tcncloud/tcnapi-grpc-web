ffa176fe771555efd1d1fc595ff7878dad4d082e
TCN CI <ci@tcnp3.com>
2024-07-02 20:35:47 +0000

    update tcnapi-grpc-web > Merge branch '602-translations-management-api-endpoints' into 'master'
    Resolve "translations management API endpoints"
    
    Closes #602
    
    See merge request tcnpublic/api!659

--- src/services/translations/v1alpha1/service_pb_service.d.ts
+++ src/services/translations/v1alpha1/service_pb_service.d.ts
@@ -14,9 +14,79 @@ type TranslationsServiceTranslateTemplate = {
   readonly responseType: typeof services_translations_v1alpha1_entities_pb.TranslateTemplateResponse;
 };
 
+type TranslationsServiceListTranslations = {
+  readonly methodName: string;
+  readonly service: typeof TranslationsService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof services_translations_v1alpha1_entities_pb.ListTranslationsRequest;
+  readonly responseType: typeof services_translations_v1alpha1_entities_pb.ListTranslationsResponse;
+};
+
+type TranslationsServiceUpdateTranslation = {
+  readonly methodName: string;
+  readonly service: typeof TranslationsService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof services_translations_v1alpha1_entities_pb.UpdateTranslationRequest;
+  readonly responseType: typeof services_translations_v1alpha1_entities_pb.UpdateTranslationResponse;
+};
+
+type TranslationsServiceTriggerLLMTranslation = {
+  readonly methodName: string;
+  readonly service: typeof TranslationsService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof services_translations_v1alpha1_entities_pb.TriggerLLMTranslationRequest;
+  readonly responseType: typeof services_translations_v1alpha1_entities_pb.TriggerLLMTranslationResponse;
+};
+
+type TranslationsServiceTriggerLLMTranslations = {
+  readonly methodName: string;
+  readonly service: typeof TranslationsService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof services_translations_v1alpha1_entities_pb.TriggerLLMTranslationsRequest;
+  readonly responseType: typeof services_translations_v1alpha1_entities_pb.TriggerLLMTranslationsResponse;
+};
+
+type TranslationsServiceSetSystemMessage = {
+  readonly methodName: string;
+  readonly service: typeof TranslationsService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof services_translations_v1alpha1_entities_pb.SetSystemMessageRequest;
+  readonly responseType: typeof services_translations_v1alpha1_entities_pb.SetSystemMessageResponse;
+};
+
+type TranslationsServiceGetSystemMessage = {
+  readonly methodName: string;
+  readonly service: typeof TranslationsService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof services_translations_v1alpha1_entities_pb.GetSystemMessageRequest;
+  readonly responseType: typeof services_translations_v1alpha1_entities_pb.GetSystemMessageResponse;
+};
+
+type TranslationsServiceTestSystemMessage = {
+  readonly methodName: string;
+  readonly service: typeof TranslationsService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof services_translations_v1alpha1_entities_pb.TestSystemMessageRequest;
+  readonly responseType: typeof services_translations_v1alpha1_entities_pb.TestSystemMessageResponse;
+};
+
 export class TranslationsService {
   static readonly serviceName: string;
   static readonly TranslateTemplate: TranslationsServiceTranslateTemplate;
+  static readonly ListTranslations: TranslationsServiceListTranslations;
+  static readonly UpdateTranslation: TranslationsServiceUpdateTranslation;
+  static readonly TriggerLLMTranslation: TranslationsServiceTriggerLLMTranslation;
+  static readonly TriggerLLMTranslations: TranslationsServiceTriggerLLMTranslations;
+  static readonly SetSystemMessage: TranslationsServiceSetSystemMessage;
+  static readonly GetSystemMessage: TranslationsServiceGetSystemMessage;
+  static readonly TestSystemMessage: TranslationsServiceTestSystemMessage;
 }
 
 export type ServiceError = { message: string, code: number; metadata: grpc.Metadata }
@@ -59,6 +129,69 @@ export class TranslationsServiceClient {
   translateTemplate(
     requestMessage: services_translations_v1alpha1_entities_pb.TranslateTemplateRequest,
     callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.TranslateTemplateResponse|null) => void
+  ): UnaryResponse;
+  listTranslations(
+    requestMessage: services_translations_v1alpha1_entities_pb.ListTranslationsRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.ListTranslationsResponse|null) => void
+  ): UnaryResponse;
+  listTranslations(
+    requestMessage: services_translations_v1alpha1_entities_pb.ListTranslationsRequest,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.ListTranslationsResponse|null) => void
+  ): UnaryResponse;
+  updateTranslation(
+    requestMessage: services_translations_v1alpha1_entities_pb.UpdateTranslationRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.UpdateTranslationResponse|null) => void
+  ): UnaryResponse;
+  updateTranslation(
+    requestMessage: services_translations_v1alpha1_entities_pb.UpdateTranslationRequest,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.UpdateTranslationResponse|null) => void
+  ): UnaryResponse;
+  triggerLLMTranslation(
+    requestMessage: services_translations_v1alpha1_entities_pb.TriggerLLMTranslationRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.TriggerLLMTranslationResponse|null) => void
+  ): UnaryResponse;
+  triggerLLMTranslation(
+    requestMessage: services_translations_v1alpha1_entities_pb.TriggerLLMTranslationRequest,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.TriggerLLMTranslationResponse|null) => void
+  ): UnaryResponse;
+  triggerLLMTranslations(
+    requestMessage: services_translations_v1alpha1_entities_pb.TriggerLLMTranslationsRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.TriggerLLMTranslationsResponse|null) => void
+  ): UnaryResponse;
+  triggerLLMTranslations(
+    requestMessage: services_translations_v1alpha1_entities_pb.TriggerLLMTranslationsRequest,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.TriggerLLMTranslationsResponse|null) => void
+  ): UnaryResponse;
+  setSystemMessage(
+    requestMessage: services_translations_v1alpha1_entities_pb.SetSystemMessageRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.SetSystemMessageResponse|null) => void
+  ): UnaryResponse;
+  setSystemMessage(
+    requestMessage: services_translations_v1alpha1_entities_pb.SetSystemMessageRequest,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.SetSystemMessageResponse|null) => void
+  ): UnaryResponse;
+  getSystemMessage(
+    requestMessage: services_translations_v1alpha1_entities_pb.GetSystemMessageRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.GetSystemMessageResponse|null) => void
+  ): UnaryResponse;
+  getSystemMessage(
+    requestMessage: services_translations_v1alpha1_entities_pb.GetSystemMessageRequest,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.GetSystemMessageResponse|null) => void
+  ): UnaryResponse;
+  testSystemMessage(
+    requestMessage: services_translations_v1alpha1_entities_pb.TestSystemMessageRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.TestSystemMessageResponse|null) => void
+  ): UnaryResponse;
+  testSystemMessage(
+    requestMessage: services_translations_v1alpha1_entities_pb.TestSystemMessageRequest,
+    callback: (error: ServiceError|null, responseMessage: services_translations_v1alpha1_entities_pb.TestSystemMessageResponse|null) => void
   ): UnaryResponse;
 }
 
--- src/services/translations/v1alpha1/service_pb_service.js
+++ src/services/translations/v1alpha1/service_pb_service.js
@@ -20,6 +20,69 @@ TranslationsService.TranslateTemplate = {
   responseType: services_translations_v1alpha1_entities_pb.TranslateTemplateResponse
 };
 
+TranslationsService.ListTranslations = {
+  methodName: "ListTranslations",
+  service: TranslationsService,
+  requestStream: false,
+  responseStream: false,
+  requestType: services_translations_v1alpha1_entities_pb.ListTranslationsRequest,
+  responseType: services_translations_v1alpha1_entities_pb.ListTranslationsResponse
+};
+
+TranslationsService.UpdateTranslation = {
+  methodName: "UpdateTranslation",
+  service: TranslationsService,
+  requestStream: false,
+  responseStream: false,
+  requestType: services_translations_v1alpha1_entities_pb.UpdateTranslationRequest,
+  responseType: services_translations_v1alpha1_entities_pb.UpdateTranslationResponse
+};
+
+TranslationsService.TriggerLLMTranslation = {
+  methodName: "TriggerLLMTranslation",
+  service: TranslationsService,
+  requestStream: false,
+  responseStream: false,
+  requestType: services_translations_v1alpha1_entities_pb.TriggerLLMTranslationRequest,
+  responseType: services_translations_v1alpha1_entities_pb.TriggerLLMTranslationResponse
+};
+
+TranslationsService.TriggerLLMTranslations = {
+  methodName: "TriggerLLMTranslations",
+  service: TranslationsService,
+  requestStream: false,
+  responseStream: false,
+  requestType: services_translations_v1alpha1_entities_pb.TriggerLLMTranslationsRequest,
+  responseType: services_translations_v1alpha1_entities_pb.TriggerLLMTranslationsResponse
+};
+
+TranslationsService.SetSystemMessage = {
+  methodName: "SetSystemMessage",
+  service: TranslationsService,
+  requestStream: false,
+  responseStream: false,
+  requestType: services_translations_v1alpha1_entities_pb.SetSystemMessageRequest,
+  responseType: services_translations_v1alpha1_entities_pb.SetSystemMessageResponse
+};
+
+TranslationsService.GetSystemMessage = {
+  methodName: "GetSystemMessage",
+  service: TranslationsService,
+  requestStream: false,
+  responseStream: false,
+  requestType: services_translations_v1alpha1_entities_pb.GetSystemMessageRequest,
+  responseType: services_translations_v1alpha1_entities_pb.GetSystemMessageResponse
+};
+
+TranslationsService.TestSystemMessage = {
+  methodName: "TestSystemMessage",
+  service: TranslationsService,
+  requestStream: false,
+  responseStream: false,
+  requestType: services_translations_v1alpha1_entities_pb.TestSystemMessageRequest,
+  responseType: services_translations_v1alpha1_entities_pb.TestSystemMessageResponse
+};
+
 exports.TranslationsService = TranslationsService;
 
 function TranslationsServiceClient(serviceHost, options) {
@@ -32,6 +95,223 @@ TranslationsServiceClient.prototype.translateTemplate = function translateTempla
     callback = arguments[1];
   }
   var client = grpc.unary(TranslationsService.TranslateTemplate, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+TranslationsServiceClient.prototype.listTranslations = function listTranslations(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(TranslationsService.ListTranslations, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+TranslationsServiceClient.prototype.updateTranslation = function updateTranslation(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(TranslationsService.UpdateTranslation, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+TranslationsServiceClient.prototype.triggerLLMTranslation = function triggerLLMTranslation(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(TranslationsService.TriggerLLMTranslation, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+TranslationsServiceClient.prototype.triggerLLMTranslations = function triggerLLMTranslations(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(TranslationsService.TriggerLLMTranslations, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+TranslationsServiceClient.prototype.setSystemMessage = function setSystemMessage(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(TranslationsService.SetSystemMessage, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+TranslationsServiceClient.prototype.getSystemMessage = function getSystemMessage(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(TranslationsService.GetSystemMessage, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+TranslationsServiceClient.prototype.testSystemMessage = function testSystemMessage(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(TranslationsService.TestSystemMessage, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
