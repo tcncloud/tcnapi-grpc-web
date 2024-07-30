50f0758a20aa6aca33892798a2634b903257c961
TCN CI <ci@tcnp3.com>
2024-07-30 20:53:52 +0000

    update tcnapi-grpc-web > Merge branch '376-omni-omni_billing-update-billing-tag-protos' into 'master'
    Resolve "Update billing tag"
    
    Closes omni/omni#376
    
    See merge request tcnpublic/api!708

--- src/services/billing/entities/v1alpha4/tags_pb.d.ts
+++ src/services/billing/entities/v1alpha4/tags_pb.d.ts
@@ -26,6 +26,9 @@ export class BillingTag extends jspb.Message {
   getDeleteTime(): google_protobuf_timestamp_pb.Timestamp | undefined;
   setDeleteTime(value?: google_protobuf_timestamp_pb.Timestamp): void;
 
+  getCategory(): string;
+  setCategory(value: string): void;
+
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): BillingTag.AsObject;
   static toObject(includeInstance: boolean, msg: BillingTag): BillingTag.AsObject;
@@ -43,6 +46,7 @@ export namespace BillingTag {
     createTime?: google_protobuf_timestamp_pb.Timestamp.AsObject,
     updateTime?: google_protobuf_timestamp_pb.Timestamp.AsObject,
     deleteTime?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+    category: string,
   }
 }
 
