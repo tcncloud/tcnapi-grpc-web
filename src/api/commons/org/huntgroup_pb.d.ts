9746bd75d9a5fc969c6ffad9b0aebcba95eff824
TCN CI <ci@tcnp3.com>
2024-07-31 15:32:26 +0000

    update tcnapi-grpc-web > TCN Public API #637.  Exile link proto message.

--- src/api/commons/org/huntgroup_pb.d.ts
+++ src/api/commons/org/huntgroup_pb.d.ts
@@ -2518,6 +2518,110 @@ export namespace IntegrationLinkSubParameterPart {
   }
 }
 
+export class ExileLink extends jspb.Message {
+  getParameterSid(): number;
+  setParameterSid(value: number): void;
+
+  getHuntGroupSid(): number;
+  setHuntGroupSid(value: number): void;
+
+  getName(): string;
+  setName(value: string): void;
+
+  getDescription(): string;
+  setDescription(value: string): void;
+
+  getOrder(): number;
+  setOrder(value: number): void;
+
+  hasInboundData(): boolean;
+  clearInboundData(): void;
+  getInboundData(): ExileLinkData | undefined;
+  setInboundData(value?: ExileLinkData): void;
+
+  hasOutboundData(): boolean;
+  clearOutboundData(): void;
+  getOutboundData(): ExileLinkData | undefined;
+  setOutboundData(value?: ExileLinkData): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ExileLink.AsObject;
+  static toObject(includeInstance: boolean, msg: ExileLink): ExileLink.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ExileLink, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ExileLink;
+  static deserializeBinaryFromReader(message: ExileLink, reader: jspb.BinaryReader): ExileLink;
+}
+
+export namespace ExileLink {
+  export type AsObject = {
+    parameterSid: number,
+    huntGroupSid: number,
+    name: string,
+    description: string,
+    order: number,
+    inboundData?: ExileLinkData.AsObject,
+    outboundData?: ExileLinkData.AsObject,
+  }
+}
+
+export class ExileLinkData extends jspb.Message {
+  hasRecordId(): boolean;
+  clearRecordId(): void;
+  getRecordId(): ExileLinkParameter | undefined;
+  setRecordId(value?: ExileLinkParameter): void;
+
+  hasAlternateId(): boolean;
+  clearAlternateId(): void;
+  getAlternateId(): ExileLinkParameter | undefined;
+  setAlternateId(value?: ExileLinkParameter): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ExileLinkData.AsObject;
+  static toObject(includeInstance: boolean, msg: ExileLinkData): ExileLinkData.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ExileLinkData, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ExileLinkData;
+  static deserializeBinaryFromReader(message: ExileLinkData, reader: jspb.BinaryReader): ExileLinkData;
+}
+
+export namespace ExileLinkData {
+  export type AsObject = {
+    recordId?: ExileLinkParameter.AsObject,
+    alternateId?: ExileLinkParameter.AsObject,
+  }
+}
+
+export class ExileLinkParameter extends jspb.Message {
+  getContactFieldSid(): number;
+  setContactFieldSid(value: number): void;
+
+  getHelperValue(): string;
+  setHelperValue(value: string): void;
+
+  getParameterSourceType(): ParameterSourceTypeMap[keyof ParameterSourceTypeMap];
+  setParameterSourceType(value: ParameterSourceTypeMap[keyof ParameterSourceTypeMap]): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ExileLinkParameter.AsObject;
+  static toObject(includeInstance: boolean, msg: ExileLinkParameter): ExileLinkParameter.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ExileLinkParameter, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ExileLinkParameter;
+  static deserializeBinaryFromReader(message: ExileLinkParameter, reader: jspb.BinaryReader): ExileLinkParameter;
+}
+
+export namespace ExileLinkParameter {
+  export type AsObject = {
+    contactFieldSid: number,
+    helperValue: string,
+    parameterSourceType: ParameterSourceTypeMap[keyof ParameterSourceTypeMap],
+  }
+}
+
 export class AgentTrigger extends jspb.Message {
   getAgentTriggerSid(): number;
   setAgentTriggerSid(value: number): void;
