f01094680401ca42cf830ff3e2e89d5c54ed2098
TCN CI <ci@tcnp3.com>
2024-08-05 15:41:08 +0000

    update tcnapi-grpc-web > initial commit

--- src/api/v1alpha1/wfm/wfm_pb_service.js
+++ src/api/v1alpha1/wfm/wfm_pb_service.js
@@ -1198,6 +1198,15 @@ WFM.ListShiftInstanceSidsForAgent = {
   responseType: api_v1alpha1_wfm_wfm_pb.ListShiftInstanceSidsForAgentRes
 };
 
+WFM.ListShiftInstanceSidsForSchedule = {
+  methodName: "ListShiftInstanceSidsForSchedule",
+  service: WFM,
+  requestStream: false,
+  responseStream: false,
+  requestType: api_v1alpha1_wfm_wfm_pb.ListShiftInstanceSidsForScheduleRequest,
+  responseType: api_v1alpha1_wfm_wfm_pb.ListShiftInstanceSidsForScheduleResponse
+};
+
 WFM.ListShiftSegmentsByShiftInstanceSids = {
   methodName: "ListShiftSegmentsByShiftInstanceSids",
   service: WFM,
@@ -5787,6 +5796,37 @@ WFMClient.prototype.listShiftInstanceSidsForAgent = function listShiftInstanceSi
     callback = arguments[1];
   }
   var client = grpc.unary(WFM.ListShiftInstanceSidsForAgent, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+WFMClient.prototype.listShiftInstanceSidsForSchedule = function listShiftInstanceSidsForSchedule(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(WFM.ListShiftInstanceSidsForSchedule, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
