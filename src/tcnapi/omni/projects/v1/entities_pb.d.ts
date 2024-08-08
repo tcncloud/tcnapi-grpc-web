8ff4aa665da2649349ad71c41e145989cc1bdb69
TCN CI <ci@tcnp3.com>
2024-08-08 19:28:23 +0000

    update tcnapi-grpc-web > format

---
+++ src/tcnapi/omni/projects/v1/entities_pb.d.ts
@@ -0,0 +1,83 @@
+// package: tcnapi.omni.projects.v1
+// file: tcnapi/omni/projects/v1/entities.proto
+
+import * as jspb from "google-protobuf";
+import * as google_api_field_behavior_pb from "../../../../google/api/field_behavior_pb";
+import * as google_api_resource_pb from "../../../../google/api/resource_pb";
+import * as google_protobuf_field_mask_pb from "google-protobuf/google/protobuf/field_mask_pb";
+import * as tcnapi_omni_projects_v1_projects_pb from "../../../../tcnapi/omni/projects/v1/projects_pb";
+
+export class ListProjectsRequest extends jspb.Message {
+  getParent(): string;
+  setParent(value: string): void;
+
+  getPageSize(): number;
+  setPageSize(value: number): void;
+
+  getPageToken(): string;
+  setPageToken(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ListProjectsRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: ListProjectsRequest): ListProjectsRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ListProjectsRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ListProjectsRequest;
+  static deserializeBinaryFromReader(message: ListProjectsRequest, reader: jspb.BinaryReader): ListProjectsRequest;
+}
+
+export namespace ListProjectsRequest {
+  export type AsObject = {
+    parent: string,
+    pageSize: number,
+    pageToken: string,
+  }
+}
+
+export class ListProjectsResponse extends jspb.Message {
+  clearProjectsList(): void;
+  getProjectsList(): Array<tcnapi_omni_projects_v1_projects_pb.Project>;
+  setProjectsList(value: Array<tcnapi_omni_projects_v1_projects_pb.Project>): void;
+  addProjects(value?: tcnapi_omni_projects_v1_projects_pb.Project, index?: number): tcnapi_omni_projects_v1_projects_pb.Project;
+
+  getNextPageToken(): string;
+  setNextPageToken(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ListProjectsResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: ListProjectsResponse): ListProjectsResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ListProjectsResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ListProjectsResponse;
+  static deserializeBinaryFromReader(message: ListProjectsResponse, reader: jspb.BinaryReader): ListProjectsResponse;
+}
+
+export namespace ListProjectsResponse {
+  export type AsObject = {
+    projectsList: Array<tcnapi_omni_projects_v1_projects_pb.Project.AsObject>,
+    nextPageToken: string,
+  }
+}
+
+export class GetProjectRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetProjectRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: GetProjectRequest): GetProjectRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: GetProjectRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetProjectRequest;
+  static deserializeBinaryFromReader(message: GetProjectRequest, reader: jspb.BinaryReader): GetProjectRequest;
+}
+
+export namespace GetProjectRequest {
+  export type AsObject = {
+    name: string,
+  }
+}
+
---
+++ src/tcnapi/omni/projects/v1/entities_pb_service.d.ts
@@ -0,0 +1,3 @@
+// package: tcnapi.omni.projects.v1
+// file: tcnapi/omni/projects/v1/entities.proto
+
