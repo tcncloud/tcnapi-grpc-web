d003627abd7da697ad3411bdade8bc747f050c7c
TCN CI <ci@tcnp3.com>
2024-09-10 22:01:13 +0000

    update tcnapi-grpc-web > Merge branch '693-add-public-protos-for-list-methods-for-adherence-rule-entities' into 'master'
    Resolve "Add public protos for List methods for adherence rule entities"
    
    Closes wfm/wfm#146 and #693
    
    See merge request tcnpublic/api!788

--- src/services/org/hunt_groups/v1alpha1/service_pb_service.d.ts
+++ src/services/org/hunt_groups/v1alpha1/service_pb_service.d.ts
@@ -59,15 +59,6 @@ type HuntGroupsServiceUpdateHuntGroupAgentTriggers = {
   readonly responseType: typeof services_org_hunt_groups_v1alpha1_entities_pb.UpdateHuntGroupAgentTriggersResponse;
 };
 
-type HuntGroupsServiceCopyHuntGroupToOrganization = {
-  readonly methodName: string;
-  readonly service: typeof HuntGroupsService;
-  readonly requestStream: false;
-  readonly responseStream: false;
-  readonly requestType: typeof services_org_hunt_groups_v1alpha1_entities_pb.CopyHuntGroupToOrganizationRequest;
-  readonly responseType: typeof services_org_hunt_groups_v1alpha1_entities_pb.CopyHuntGroupToOrganizationResponse;
-};
-
 export class HuntGroupsService {
   static readonly serviceName: string;
   static readonly ListHuntGroupExileLinks: HuntGroupsServiceListHuntGroupExileLinks;
@@ -76,7 +67,6 @@ export class HuntGroupsService {
   static readonly ListHuntGroupAgentTriggers: HuntGroupsServiceListHuntGroupAgentTriggers;
   static readonly CopyHuntGroupAgentTrigger: HuntGroupsServiceCopyHuntGroupAgentTrigger;
   static readonly UpdateHuntGroupAgentTriggers: HuntGroupsServiceUpdateHuntGroupAgentTriggers;
-  static readonly CopyHuntGroupToOrganization: HuntGroupsServiceCopyHuntGroupToOrganization;
 }
 
 export type ServiceError = { message: string, code: number; metadata: grpc.Metadata }
@@ -164,15 +154,6 @@ export class HuntGroupsServiceClient {
   updateHuntGroupAgentTriggers(
     requestMessage: services_org_hunt_groups_v1alpha1_entities_pb.UpdateHuntGroupAgentTriggersRequest,
     callback: (error: ServiceError|null, responseMessage: services_org_hunt_groups_v1alpha1_entities_pb.UpdateHuntGroupAgentTriggersResponse|null) => void
-  ): UnaryResponse;
-  copyHuntGroupToOrganization(
-    requestMessage: services_org_hunt_groups_v1alpha1_entities_pb.CopyHuntGroupToOrganizationRequest,
-    metadata: grpc.Metadata,
-    callback: (error: ServiceError|null, responseMessage: services_org_hunt_groups_v1alpha1_entities_pb.CopyHuntGroupToOrganizationResponse|null) => void
-  ): UnaryResponse;
-  copyHuntGroupToOrganization(
-    requestMessage: services_org_hunt_groups_v1alpha1_entities_pb.CopyHuntGroupToOrganizationRequest,
-    callback: (error: ServiceError|null, responseMessage: services_org_hunt_groups_v1alpha1_entities_pb.CopyHuntGroupToOrganizationResponse|null) => void
   ): UnaryResponse;
 }
 
