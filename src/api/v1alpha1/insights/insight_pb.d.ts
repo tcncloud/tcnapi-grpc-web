859e56b605f7c0fb9a61d0a70cf854d64ab6ba37
TCN CI <ci@tcnp3.com>
2024-10-21 16:53:45 +0000

    update tcnapi-grpc-web > Merge branch '732-fix-proto-export' into 'master'
    Resolve "Fix Proto Export"
    
    Closes #732
    
    See merge request tcnpublic/api!833

--- src/api/v1alpha1/insights/insight_pb.d.ts
+++ src/api/v1alpha1/insights/insight_pb.d.ts
@@ -540,12 +540,6 @@ export class TableVisualization extends jspb.Message {
   setTableColumnDetailsList(value: Array<TableColumnConfig>): void;
   addTableColumnDetails(value?: TableColumnConfig, index?: number): TableColumnConfig;
 
-  getDelimiter(): string;
-  setDelimiter(value: string): void;
-
-  getQuoteCharacter(): QuoteCharacterMap[keyof QuoteCharacterMap];
-  setQuoteCharacter(value: QuoteCharacterMap[keyof QuoteCharacterMap]): void;
-
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): TableVisualization.AsObject;
   static toObject(includeInstance: boolean, msg: TableVisualization): TableVisualization.AsObject;
@@ -559,8 +553,6 @@ export class TableVisualization extends jspb.Message {
 export namespace TableVisualization {
   export type AsObject = {
     tableColumnDetailsList: Array<TableColumnConfig.AsObject>,
-    delimiter: string,
-    quoteCharacter: QuoteCharacterMap[keyof QuoteCharacterMap],
   }
 }
 
@@ -1264,12 +1256,4 @@ export interface InsightContextualActionTypeMap {
 }
 
 export const InsightContextualActionType: InsightContextualActionTypeMap;
-
-export interface QuoteCharacterMap {
-  QUOTE_CHARACTER_UNSPECIFIED: 0;
-  QUOTE_CHARACTER_DOUBLE_QUOTE: 1;
-  QUOTE_CHARACTER_SINGLE_QUOTE: 2;
-}
-
-export const QuoteCharacter: QuoteCharacterMap;
 
