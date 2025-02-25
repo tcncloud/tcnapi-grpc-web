57ed9fd333a457e5d67e044d88ee9b85e6479830
TCN CI <ci@tcnp3.com>
2025-02-25 05:54:49 +0000

    update tcnapi-grpc-web > updating enum_types

--- src/api/commons/audit/tickets_events_pb.d.ts
+++ src/api/commons/audit/tickets_events_pb.d.ts
@@ -13,11 +13,6 @@ export class TicketEvent extends jspb.Message {
   getCreatedById(): string;
   setCreatedById(value: string): void;
 
-  hasTicketCustomFieldAuditLog(): boolean;
-  clearTicketCustomFieldAuditLog(): void;
-  getTicketCustomFieldAuditLog(): api_commons_tickets_pb.TicketCustomFieldAuditLog | undefined;
-  setTicketCustomFieldAuditLog(value?: api_commons_tickets_pb.TicketCustomFieldAuditLog): void;
-
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): TicketEvent.AsObject;
   static toObject(includeInstance: boolean, msg: TicketEvent): TicketEvent.AsObject;
@@ -32,7 +27,58 @@ export namespace TicketEvent {
   export type AsObject = {
     editticket?: api_commons_tickets_pb.EditTicket.AsObject,
     createdById: string,
+  }
+}
+
+export class TicketCustomFieldCreateEvent extends jspb.Message {
+  hasTicketCustomFieldAuditLog(): boolean;
+  clearTicketCustomFieldAuditLog(): void;
+  getTicketCustomFieldAuditLog(): api_commons_tickets_pb.TicketCustomFieldAuditLog | undefined;
+  setTicketCustomFieldAuditLog(value?: api_commons_tickets_pb.TicketCustomFieldAuditLog): void;
+
+  getCreatedById(): string;
+  setCreatedById(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): TicketCustomFieldCreateEvent.AsObject;
+  static toObject(includeInstance: boolean, msg: TicketCustomFieldCreateEvent): TicketCustomFieldCreateEvent.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: TicketCustomFieldCreateEvent, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): TicketCustomFieldCreateEvent;
+  static deserializeBinaryFromReader(message: TicketCustomFieldCreateEvent, reader: jspb.BinaryReader): TicketCustomFieldCreateEvent;
+}
+
+export namespace TicketCustomFieldCreateEvent {
+  export type AsObject = {
     ticketCustomFieldAuditLog?: api_commons_tickets_pb.TicketCustomFieldAuditLog.AsObject,
+    createdById: string,
+  }
+}
+
+export class TicketCustomFieldEditEvent extends jspb.Message {
+  hasTicketCustomFieldAuditLog(): boolean;
+  clearTicketCustomFieldAuditLog(): void;
+  getTicketCustomFieldAuditLog(): api_commons_tickets_pb.TicketCustomFieldAuditLog | undefined;
+  setTicketCustomFieldAuditLog(value?: api_commons_tickets_pb.TicketCustomFieldAuditLog): void;
+
+  getCreatedById(): string;
+  setCreatedById(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): TicketCustomFieldEditEvent.AsObject;
+  static toObject(includeInstance: boolean, msg: TicketCustomFieldEditEvent): TicketCustomFieldEditEvent.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: TicketCustomFieldEditEvent, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): TicketCustomFieldEditEvent;
+  static deserializeBinaryFromReader(message: TicketCustomFieldEditEvent, reader: jspb.BinaryReader): TicketCustomFieldEditEvent;
+}
+
+export namespace TicketCustomFieldEditEvent {
+  export type AsObject = {
+    ticketCustomFieldAuditLog?: api_commons_tickets_pb.TicketCustomFieldAuditLog.AsObject,
+    createdById: string,
   }
 }
 
