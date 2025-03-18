95b2d85b30ccca9628762bc0ca1dfe581b5d44c3
TCN CI <ci@tcnp3.com>
2025-03-18 20:54:57 +0000

    update tcnapi-grpc-web > Merge branch '890-knowledgeretriever-passthrough-endpoints-for-learn' into 'master'
    Resolve "KnowledgeRetriever passthrough endpoints for learn"
    
    Closes #890
    
    See merge request tcnpublic/api!1038

--- src/api/v0alpha/learn_pb_service.js
+++ src/api/v0alpha/learn_pb_service.js
@@ -244,6 +244,15 @@ Learn.GetUpdateUrl = {
   responseType: api_v0alpha_learn_pb.GetUpdateUrlRes
 };
 
+Learn.AdvancedSearch = {
+  methodName: "AdvancedSearch",
+  service: Learn,
+  requestStream: false,
+  responseStream: false,
+  requestType: api_v0alpha_learn_pb.AdvancedSearchReq,
+  responseType: api_v0alpha_learn_pb.AdvancedSearchRes
+};
+
 exports.Learn = Learn;
 
 function LearnClient(serviceHost, options) {
@@ -1063,6 +1072,37 @@ LearnClient.prototype.getUpdateUrl = function getUpdateUrl(requestMessage, metad
     callback = arguments[1];
   }
   var client = grpc.unary(Learn.GetUpdateUrl, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+LearnClient.prototype.advancedSearch = function advancedSearch(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Learn.AdvancedSearch, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
