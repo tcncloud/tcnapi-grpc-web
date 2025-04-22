6be0d402d3ab1af81c5f2b5ff6b23daa5ee3696d
TCN CI <ci@tcnp3.com>
2025-04-22 15:17:40 +0000

    update tcnapi-grpc-web > Included country code to AddContactEntryRequest...

--- src/api/v1alpha1/contactmanager/contactmanager_pb.d.ts
+++ src/api/v1alpha1/contactmanager/contactmanager_pb.d.ts
@@ -447,6 +447,9 @@ export class AddContactEntryRequest extends jspb.Message {
   setFieldList(value: Array<ContactField>): void;
   addField(value?: ContactField, index?: number): ContactField;
 
+  getCountryCode(): string;
+  setCountryCode(value: string): void;
+
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): AddContactEntryRequest.AsObject;
   static toObject(includeInstance: boolean, msg: AddContactEntryRequest): AddContactEntryRequest.AsObject;
@@ -463,6 +466,7 @@ export namespace AddContactEntryRequest {
     entryList: Array<Entry.AsObject>,
     projectSid: string,
     fieldList: Array<ContactField.AsObject>,
+    countryCode: string,
   }
 }
 
